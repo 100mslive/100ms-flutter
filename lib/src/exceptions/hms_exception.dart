f8144328e7088415d01c08a8b50231db769a64ca
Yogesh Singh <yogesh@100ms.live>
2021-11-22 17:36:54 +0530

    removed hms_error.dart file

--- lib/src/exceptions/hms_exception.dart
+++ lib/src/exceptions/hms_exception.dart
@@ -2,31 +2,6 @@ import 'package:hmssdk_flutter/hmssdk_flutter.dart';
 import '../exceptions/hms_exception_code.dart';
 
 class HMSException {
-  // String description;
-  // String message;
-  // int code;
-  // String action;
-  // String name;
-
-  // HMSException(
-  //     this.description, this.message, this.code, this.action, this.name);
-
-  // factory HMSException.fromMap(Map map) {
-  //   map = map["error"];
-  //   return HMSException(
-  //     map['description'],
-  //     map['message'],
-  //     map['code'],
-  //     map['action'],
-  //     map['name'],
-  //   );
-  // }
-
-  // @override
-  // String toString() {
-  //   return 'HMSException{description: $description, message: $message, code: $code, action: $action, name: $name}';
-  // }
-
   final String? id;
   final HMSExceptionCode? code;
   final String message;
