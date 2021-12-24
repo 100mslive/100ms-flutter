3afbc9c31177c4e6b624215d8db47f4f0f78e055
Yogesh Singh <yogesh@100ms.live>
2021-12-24 13:06:35 +0530

    flutter formatted

--- example/lib/logs/custom_singleton_logger.dart
+++ example/lib/logs/custom_singleton_logger.dart
@@ -58,7 +58,7 @@ class FileOutput extends LogOutput {
   @override
   void init() {
     _sink = file?.openWrite(
-      mode:FileMode.writeOnly,
+      mode: FileMode.writeOnly,
       encoding: encoding,
     );
   }
