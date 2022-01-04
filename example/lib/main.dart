8431059072fc5d44484ee09eca30244c1ca0875f
Yogesh Singh <yogesh@100ms.live>
2022-01-04 17:34:33 +0530

    updated pubspec

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'dart:io';
-
+import 'package:connectivity_checker/connectivity_checker.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/cupertino.dart';
