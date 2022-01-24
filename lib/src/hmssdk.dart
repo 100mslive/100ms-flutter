87c8c84293c79e0d93d303091fccbabe4061bf9b
Arunshaik2001 <50947867+Arunshaik2001@users.noreply.github.com>
2022-01-24 14:49:29 +0530

    Update hmssdk.dart

--- lib/src/hmssdk.dart
+++ lib/src/hmssdk.dart
@@ -5,7 +5,6 @@
 ///All methods related to meeting, preview and their listeners are present here.
 
 // Project imports:
-import 'package:flutter/material.dart';
 import 'package:hmssdk_flutter/hmssdk_flutter.dart';
 import 'package:hmssdk_flutter/src/manager/hms_sdk_manager.dart';
 import 'package:hmssdk_flutter/src/service/platform_service.dart';
