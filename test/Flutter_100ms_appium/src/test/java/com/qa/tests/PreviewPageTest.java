588263e96135be7331256970b5c93bed517c0eab
ronitroy <ronit@100ms.live>
2022-05-11 12:20:44 +0530

    changes

--- test/Flutter_100ms_appium/src/test/java/com/qa/tests/PreviewPageTest.java
+++ test/Flutter_100ms_appium/src/test/java/com/qa/tests/PreviewPageTest.java
@@ -64,7 +64,7 @@ public class PreviewPageTest extends BaseTest {
     @Test
     public void Test_PreviewPage() throws InterruptedException {
         System.out.println("Verify Preview page locators");
-        previewPage = homePage.goto_previewPage(meetingDetail.getJSONObject("valid").getString("meeting_url"), meetingDetail.getJSONObject("valid").getString("username"));
+        homePage.goto_previewPage(meetingDetail.getJSONObject("valid").getString("meeting_url"), meetingDetail.getJSONObject("valid").getString("username"));
         Thread.sleep(2000);
 
 //        sa.assertTrue(previewPage.videoTile.isDisplayed());
