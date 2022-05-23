7d4fb64fb4ce15014973465ef5b97475e3dbbf6c
Decoder07 <pushpam@100ms.live>
2022-05-23 13:07:29 +0530

    Removed RTC Stats listeners from update Listeners

--- lib/src/model/hms_update_listener.dart
+++ lib/src/model/hms_update_listener.dart
@@ -94,27 +94,4 @@ abstract class HMSUpdateListener {
   void onRemovedFromRoom(
       {required HMSPeerRemovedFromPeer hmsPeerRemovedFromPeer});
 
-  void onLocalAudioStats(
-      {required HMSLocalAudioStats hmsLocalAudioStats,
-      required HMSLocalAudioTrack track,
-      required HMSPeer peer});
-
-  void onLocalVideoStats(
-      {required HMSLocalVideoStats hmsLocalVideoStats,
-      required HMSLocalVideoTrack track,
-      required HMSPeer peer});
-
-  void onRemoteAudioStats(
-      {required HMSRemoteAudioStats hmsRemoteAudioStats,
-      required HMSRemoteAudioTrack track,
-      required HMSPeer peer});
-
-  void onRemoteVideoStats(
-      {required HMSRemoteVideoStats hmsRemoteVideoStats,
-      required HMSRemoteVideoTrack track,
-      required HMSPeer peer});
-
-  void onRTCStats({
-    required HMSRTCStatsReport hmsrtcStatsReport,
-  });
 }
