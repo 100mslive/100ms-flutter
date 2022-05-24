c9d01d2a98d45144b60f54915b4eb78c66a6a78d
govindmaheshwari2 <govindmah14@gmail.com>
2022-05-24 12:42:32 +0530

    remove peerName field from HMSVideoView

--- example/lib/common/ui/organisms/video_view.dart
+++ example/lib/common/ui/organisms/video_view.dart
@@ -57,7 +57,6 @@ class _VideoViewState extends State<VideoView> {
                       child: HMSVideoView(
                           scaleType: widget.scaleType,
                           track: data.item1!,
-                          peerName: context.read<PeerTrackNode>().peer.name,
                           setMirror: false,
                           matchParent: false),
                     ),
@@ -72,7 +71,6 @@ class _VideoViewState extends State<VideoView> {
                       child: HMSVideoView(
                         scaleType: ScaleType.SCALE_ASPECT_FILL,
                         track: data.item1!,
-                        peerName: context.read<PeerTrackNode>().peer.name,
                         setMirror: data.item1.runtimeType == HMSLocalVideoTrack
                             ? context.read<MeetingStore>().isMirror
                             : false,
