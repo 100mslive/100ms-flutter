a3a809ab3d3a6dc4946de926b1201964a2e58f4f
Decoder07 <pushpam@100ms.live>
2022-06-24 18:22:45 +0530

    Added Button

---
+++ example/lib/common/ui/organisms/embedded_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+
+class EmbeddedButton extends StatelessWidget {
+  final void Function() onTap;
+  final Color offColor;
+  final Color onColor;
+  final bool isActive;
+  final Widget child;
+  final double? height;
+  final double? width;
+  EmbeddedButton(
+      {required this.onTap,
+      required this.offColor,
+      required this.onColor,
+      required this.isActive,
+      required this.child,
+      this.height = 50,
+      this.width=50});
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+          width: width,
+          height: height,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(12)),
+              color: isActive ? onColor : offColor),
+          child: child),
+    );
+  }
+}
