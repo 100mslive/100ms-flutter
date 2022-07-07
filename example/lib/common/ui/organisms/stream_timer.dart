48f41058f6063e0ab3af5a951fba28c172b13fbb
Govind <govind@Govinds-MacBook-Pro.local>
2022-07-07 18:53:56 +0530

    stream timer fix

--- example/lib/common/ui/organisms/stream_timer.dart
+++ example/lib/common/ui/organisms/stream_timer.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:hmssdk_flutter_example/common/util/app_color.dart';
 import 'package:hmssdk_flutter_example/hls-streaming/util/hls_subtitle_text.dart';
@@ -24,9 +26,14 @@ class _StreamTimerState extends State<StreamTimer> {
     return StreamBuilder(
       stream: Stream.periodic(const Duration(seconds: 1)),
       builder: (context, snapshot) {
+        if (Platform.isIOS)
+          return HLSSubtitleText(
+              text: format(DateTime.now().toUtc().difference(
+                  widget.startedAt.add(DateTime.now().timeZoneOffset))),
+              textColor: subHeadingColor);
         return HLSSubtitleText(
-            text: format(DateTime.now().toUtc().difference(
-                widget.startedAt.add(DateTime.now().timeZoneOffset))),
+            text: format(
+                DateTime.now().toUtc().difference(widget.startedAt.toUtc())),
             textColor: subHeadingColor);
       },
     );
