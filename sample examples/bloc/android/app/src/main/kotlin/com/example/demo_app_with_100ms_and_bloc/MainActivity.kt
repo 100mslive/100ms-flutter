7dee6a63d66d6244bb3c3914fbe54b7723aab8cc
Govind <govind@Govinds-MacBook-Pro.local>
2022-07-18 15:11:29 +0530

    change folder name

--- sample examples/bloc/android/app/src/main/kotlin/com/example/demo_app_with_100ms_and_bloc/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.demo_app_with_100ms_and_bloc
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- sample examples/mobx/android/app/src/main/kotlin/com/example/zoom/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.mobx
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
