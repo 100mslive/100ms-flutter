50e00903d6c298c8d67aa4519de8969a756fc114
Decoder07 <pushpam@100ms.live>
2022-10-18 18:20:03 +0530

    Fixed track setting fix in iOS

--- example/lib/hms_app_settings.dart
+++ example/lib/hms_app_settings.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -136,6 +138,7 @@ class _HMSAppSettingsState extends State<HMSAppSettings> {
                   //             setState(() {})
                   //           })),
                   // ),
+                  if(Platform.isAndroid)
                   ListTile(
                     horizontalTitleGap: 2,
                     enabled: false,
@@ -164,6 +167,7 @@ class _HMSAppSettingsState extends State<HMSAppSettings> {
                               setState(() {})
                             }),
                   ),
+                  if(Platform.isAndroid)
                   ListTile(
                     horizontalTitleGap: 2,
                     enabled: false,
