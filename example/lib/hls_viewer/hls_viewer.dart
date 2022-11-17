3a2639d8b8315ebaa8d39709a771f36d85c40fe6
Yogesh Singh <yogesh@100ms.live>
2022-11-17 21:55:47 +0530

    updated HLS player

--- example/lib/hls_viewer/hls_viewer.dart
+++ example/lib/hls_viewer/hls_viewer.dart
@@ -4,6 +4,11 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:hmssdk_flutter_example/common/util/app_color.dart';
 import 'package:hmssdk_flutter_example/hls-streaming/util/hls_title_text.dart';
+import 'package:pip_flutter/pipflutter_player.dart';
+import 'package:pip_flutter/pipflutter_player_configuration.dart';
+import 'package:pip_flutter/pipflutter_player_controller.dart';
+import 'package:pip_flutter/pipflutter_player_data_source.dart';
+import 'package:pip_flutter/pipflutter_player_data_source_type.dart';
 import 'package:provider/provider.dart';
 import 'package:video_player/video_player.dart';
 
@@ -23,20 +28,42 @@ class _HLSPlayerState extends State<HLSPlayer> {
   @override
   void initState() {
     super.initState();
-    context.read<MeetingStore>().hlsVideoController =
-        VideoPlayerController.network(
+
+    PipFlutterPlayerConfiguration pipFlutterPlayerConfiguration =
+        const PipFlutterPlayerConfiguration(
+      aspectRatio: 16 / 9,
+      fit: BoxFit.contain,
+    );
+    PipFlutterPlayerDataSource dataSource = PipFlutterPlayerDataSource(
+      PipFlutterPlayerDataSourceType.network,
       widget.streamUrl,
-    )..initialize().then((_) {
-            context.read<MeetingStore>().hlsVideoController!.play();
-            setState(() {});
-          });
+    );
+    context.read<MeetingStore>().hlsVideoController =
+        PipFlutterPlayerController(pipFlutterPlayerConfiguration);
+    context
+        .read<MeetingStore>()
+        .hlsVideoController!
+        .setupDataSource(dataSource);
+    context
+        .read<MeetingStore>()
+        .hlsVideoController!
+        .setPipFlutterPlayerGlobalKey(
+            context.read<MeetingStore>().pipFlutterPlayerKey);
+
+    // context.read<MeetingStore>().hlsVideoController =
+    //     VideoPlayerController.network(
+    //   widget.streamUrl,
+    // )..initialize().then((_) {
+    //         context.read<MeetingStore>().hlsVideoController!.play();
+    //         setState(() {});
+    //       });
   }
 
   @override
   void dispose() async {
     super.dispose();
     try {
-      await context.read<MeetingStore>().hlsVideoController?.dispose();
+      // await context.read<MeetingStore>().hlsVideoController?.dispose();
       context.read<MeetingStore>().hlsVideoController = null;
     } catch (e) {
       //To handle the error when the user calls leave from hls-viewer role.
@@ -46,22 +73,19 @@ class _HLSPlayerState extends State<HLSPlayer> {
 
   @override
   Widget build(BuildContext context) {
-    return Selector<MeetingStore, VideoPlayerController?>(
+    return Selector<MeetingStore, PipFlutterPlayerController?>(
         selector: (_, meetingStore) => meetingStore.hlsVideoController,
         builder: (_, controller, __) {
           return Scaffold(
               key: GlobalKey(),
               body: Center(
-                child: controller?.value.isInitialized ?? false
-                    ? AspectRatio(
-                        aspectRatio: controller!.value.aspectRatio,
-                        child: VideoPlayer(controller),
-                      )
-                    : HLSTitleText(
-                        text: "Waiting for the HLS Streaming to start...",
-                        textColor: themeDefaultColor,
-                      ),
-              ));
+                  child: AspectRatio(
+                aspectRatio: 16 / 9,
+                child: PipFlutterPlayer(
+                  controller: controller!,
+                  key: context.read<MeetingStore>().pipFlutterPlayerKey,
+                ),
+              )));
         });
   }
 }
