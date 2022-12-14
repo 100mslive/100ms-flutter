346fc50a768dd0e01581324458f50f78345c4013
Yogesh Singh <yogesh@100ms.live>
2022-12-14 18:43:35 +0530

    updated iOS SDK version

--- lib/src/common/constant.dart
+++ lib/src/common/constant.dart
@@ -1,6 +1,6 @@
 ///This contains info about the HMSSDK versions
 class HMSSDKConstants {
   static String androidSDKVersion = "2.5.3";
-  static String iOSSDKVersion = "0.5.2";
+  static String iOSSDKVersion = "0.5.3";
   static String hmsSDKVersion = "1.1.0";
 }
