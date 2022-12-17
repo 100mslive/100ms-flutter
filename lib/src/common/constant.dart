e81f123a5dadc1c7e67fb807b70548709ee3732d
Yogesh Singh <yogesh@100ms.live>
2022-12-17 18:10:05 +0530

    using JSON for package versioning

--- lib/src/common/constant.dart
+++ lib/src/common/constant.dart
@@ -1,6 +1,6 @@
 ///This contains info about the HMSSDK versions
 class HMSSDKConstants {
-  static String androidSDKVersion = "2.5.4";
+  static String androidSDKVersion = "2.5.5";
   static String iOSSDKVersion = "0.5.3";
-  static String hmsSDKVersion = "1.1.0";
+  static String hmsSDKVersion = "1.2.0";
 }
