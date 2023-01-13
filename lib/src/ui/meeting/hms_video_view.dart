377dc905ea3dc3a2c89059040eb5ad9f4283a744
Govind Maheshwari <45732601+govindmaheshwari2@users.noreply.github.com>
2023-01-13 15:33:16 +0530

    HMSVideoView docs (#1074)
    Co-authored-by: Govind <govind@Govinds-MacBook-Pro.local>

--- lib/src/ui/meeting/hms_video_view.dart
+++ lib/src/ui/meeting/hms_video_view.dart
@@ -16,22 +16,69 @@ import 'package:hmssdk_flutter/hmssdk_flutter.dart';
 ///
 /// just pass the videotracks of local or remote peer and internally it passes [peer_id], [is_local] and [track_id] to specific views.
 ///
-/// if you want to pass height and width you can pass as a map.
-///
 /// [HMSVideoView] will render video using trackId from HMSTrack
 ///
-/// [key] property can be used to forcefully rebuild the video widget by setting a unique key everytime.
+/// **parameters**
+///
+/// **track** - This will render video with trackId present in the track. Use video track only.
+///
+/// **matchParent** - To match the size of the parent widget.
+///
+/// **scaleType** - To set the video scaling. [SCALE_ASPECT_FIT, SCALE_ASPECT_FILL, SCALE_ASPECT_BALANCED]
+///
+/// **setMirror** - To set mirroring of video
+///
+/// **disableAutoSimulcastLayerSelect** -  To disable auto simulcast (Adaptive Bitrate)
+///
+/// **key** - [key] property can be used to forcefully rebuild the video widget by setting a unique key everytime.
 /// Similarly to avoid rebuilding the key should be kept the same for particular HMSVideoView.
 ///
 /// Refer [HMSVideoView guide here](https://www.100ms.live/docs/flutter/v2/features/render-video)
 class HMSVideoView extends StatelessWidget {
+  /// This will render video with trackId present in the track
+  /// [track] - the video track to be displayed
   final HMSVideoTrack track;
-  final matchParent;
 
+  /// [matchParent] - to match the size of the parent widget
+  final bool matchParent;
+
+  /// [scaleType] - To set the video scaling.
+  ///
+  /// ScaleType can be one of the following: [SCALE_ASPECT_FIT, SCALE_ASPECT_FILL, SCALE_ASPECT_BALANCED]
   final ScaleType scaleType;
+
+  /// [setMirror] - To set mirroring of video
   final bool setMirror;
+
+  /// [disableAutoSimulcastLayerSelect] - To disable auto simulcast (Adaptive Bitrate)
   final bool disableAutoSimulcastLayerSelect;
 
+  ///100ms HMSVideoView
+  ///
+  ///HMSVideoView used to render video in ios and android devices
+  ///
+  /// To use,import package:`hmssdk_flutter/ui/meeting/hms_video_view.dart`.
+  ///
+  /// just pass the videotracks of local or remote peer and internally it passes [peer_id], [is_local] and [track_id] to specific views.
+  ///
+  /// [HMSVideoView] will render video using trackId from HMSTrack
+  ///
+  /// **parameters**
+  ///
+  /// **track** - This will render video with trackId present in the track. Use video track only.
+  ///
+  /// **matchParent** - To match the size of the parent widget.
+  ///
+  /// **scaleType** - To set the video scaling.`[SCALE_ASPECT_FIT, SCALE_ASPECT_FILL, SCALE_ASPECT_BALANCED]`
+  ///
+  /// **setMirror** - To set mirroring of video
+  ///
+  /// **disableAutoSimulcastLayerSelect** -  To disable auto simulcast (Adaptive Bitrate)
+  ///
+  /// **key** - [key] property can be used to forcefully rebuild the video widget by setting a unique key everytime.
+  /// Similarly to avoid rebuilding the key should be kept the same for particular HMSVideoView.
+  ///
+  /// Refer [HMSVideoView guide here](https://www.100ms.live/docs/flutter/v2/features/render-video)
   HMSVideoView(
       {Key? key,
       required this.track,
