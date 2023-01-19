3eb874d2ebcb9e2158df3cdae0508d6abcde7094
Govind Maheshwari <45732601+govindmaheshwari2@users.noreply.github.com>
2023-01-19 13:58:09 +0530

    more option icon added (#1092)
    Co-authored-by: Govind <govind@Govinds-MacBook-Pro.local>

---
+++ example/lib/common/peer_widgets/more_option.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+import 'package:hmssdk_flutter/hmssdk_flutter.dart';
+import 'package:hmssdk_flutter_example/common/app_dialogs/change_role_option_dialog.dart';
+import 'package:hmssdk_flutter_example/common/app_dialogs/change_simulcast_layer_option_dialog.dart';
+import 'package:hmssdk_flutter_example/common/app_dialogs/local_peer_tile_dialog.dart';
+import 'package:hmssdk_flutter_example/common/app_dialogs/remote_peer_tile_dialog.dart';
+import 'package:hmssdk_flutter_example/common/util/utility_components.dart';
+import 'package:hmssdk_flutter_example/common/util/utility_function.dart';
+import 'package:hmssdk_flutter_example/meeting/meeting_store.dart';
+import 'package:hmssdk_flutter_example/model/peer_track_node.dart';
+import 'package:provider/provider.dart';
+
+class MoreOption extends StatelessWidget {
+  const MoreOption({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    MeetingStore _meetingStore = context.read<MeetingStore>();
+
+    bool mutePermission =
+        _meetingStore.localPeer?.role.permissions.mute ?? false;
+    bool unMutePermission =
+        _meetingStore.localPeer?.role.permissions.unMute ?? false;
+    bool removePeerPermission =
+        _meetingStore.localPeer?.role.permissions.removeOthers ?? false;
+    bool changeRolePermission =
+        _meetingStore.localPeer?.role.permissions.changeRole ?? false;
+
+    return Positioned(
+      bottom: 5,
+      right: 5,
+      child: GestureDetector(
+        onTap: () {
+          var peerTrackNode = context.read<PeerTrackNode>();
+          HMSPeer peerNode = peerTrackNode.peer;
+          if (peerTrackNode.peer.peerId != _meetingStore.localPeer!.peerId)
+            showDialog(
+                context: context,
+                builder: (_) => RemotePeerTileDialog(
+                      isAudioMuted: peerTrackNode.audioTrack?.isMute ?? true,
+                      isVideoMuted: peerTrackNode.track == null
+                          ? true
+                          : peerTrackNode.track!.isMute,
+                      peerName: peerNode.name,
+                      changeVideoTrack: (mute, isVideoTrack) {
+                        Navigator.pop(context);
+                        _meetingStore.changeTrackState(
+                            peerTrackNode.track!, mute);
+                      },
+                      changeAudioTrack: (mute, isAudioTrack) {
+                        Navigator.pop(context);
+                        _meetingStore.changeTrackState(
+                            peerTrackNode.audioTrack!, mute);
+                      },
+                      removePeer: () async {
+                        Navigator.pop(context);
+                        var peer = await _meetingStore.getPeer(
+                            peerId: peerNode.peerId);
+                        _meetingStore.removePeerFromRoom(peer!);
+                      },
+                      changeRole: () {
+                        Navigator.pop(context);
+                        showDialog(
+                            context: context,
+                            builder: (_) => ChangeRoleOptionDialog(
+                                  peerName: peerNode.name,
+                                  roles: _meetingStore.roles,
+                                  peer: peerNode,
+                                  changeRole: (role, forceChange) {
+                                    _meetingStore.changeRoleOfPeer(
+                                        peer: peerNode,
+                                        roleName: role,
+                                        forceChange: forceChange);
+                                  },
+                                ));
+                      },
+                      changeLayer: () async {
+                        Navigator.pop(context);
+                        HMSRemoteVideoTrack track =
+                            peerTrackNode.track as HMSRemoteVideoTrack;
+                        List<HMSSimulcastLayerDefinition> layerDefinitions =
+                            await track.getLayerDefinition();
+                        HMSSimulcastLayer selectedLayer =
+                            await track.getLayer();
+                        if (layerDefinitions.isNotEmpty) {
+                          showDialog(
+                              context: context,
+                              builder: (_) => ChangeSimulcastLayerOptionDialog(
+                                  layerDefinitions: layerDefinitions,
+                                  selectedLayer: selectedLayer,
+                                  track: track));
+                        } else {
+                          Utilities.showToast(
+                              "Simulcast not enabled for the role");
+                        }
+                      },
+                      mute: mutePermission,
+                      unMute: unMutePermission,
+                      removeOthers: removePeerPermission,
+                      roles: changeRolePermission,
+                      simulcast: (!(peerTrackNode.track as HMSRemoteVideoTrack)
+                          .isMute),
+                      pinTile: peerTrackNode.pinTile,
+                      changePinTileStatus: () {
+                        _meetingStore.changePinTileStatus(peerTrackNode);
+                        Navigator.pop(context);
+                      },
+                    ));
+          else
+            showDialog(
+                context: context,
+                builder: (_) => LocalPeerTileDialog(
+                    isAudioMode: false,
+                    toggleCamera: () {
+                      if (_meetingStore.isVideoOn) _meetingStore.switchCamera();
+                    },
+                    peerName: peerNode.name,
+                    changeRole: () {
+                      Navigator.pop(context);
+                      showDialog(
+                          context: context,
+                          builder: (_) => ChangeRoleOptionDialog(
+                                peerName: peerNode.name,
+                                roles: _meetingStore.roles,
+                                peer: peerNode,
+                                changeRole: (role, forceChange) {
+                                  _meetingStore.changeRoleOfPeer(
+                                      peer: peerNode,
+                                      roleName: role,
+                                      forceChange: forceChange);
+                                },
+                              ));
+                    },
+                    roles: changeRolePermission,
+                    changeName: () async {
+                      String name = await UtilityComponents.showInputDialog(
+                          context: context, placeholder: "Enter Name");
+                      if (name.isNotEmpty) {
+                        _meetingStore.changeName(name: name);
+                      }
+                    }));
+        },
+        child: Semantics(
+          label: "fl_${context.read<PeerTrackNode>().peer.name}more_option",
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(5, 0, 5, 0),
+            child: Icon(Icons.more_vert),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- example/lib/common/widgets/peer_tile.dart
+++ example/lib/common/widgets/peer_tile.dart
@@ -2,11 +2,8 @@
 import 'package:flutter/material.dart';
 import 'package:focus_detector/focus_detector.dart';
 import 'package:hmssdk_flutter/hmssdk_flutter.dart';
-import 'package:hmssdk_flutter_example/common/app_dialogs/change_role_option_dialog.dart';
-import 'package:hmssdk_flutter_example/common/app_dialogs/change_simulcast_layer_option_dialog.dart';
-import 'package:hmssdk_flutter_example/common/app_dialogs/local_peer_tile_dialog.dart';
-import 'package:hmssdk_flutter_example/common/app_dialogs/remote_peer_tile_dialog.dart';
 import 'package:hmssdk_flutter_example/common/widgets/degrade_tile.dart';
+import 'package:hmssdk_flutter_example/common/peer_widgets/more_option.dart';
 import 'package:hmssdk_flutter_example/common/widgets/video_view.dart';
 import 'package:hmssdk_flutter_example/common/peer_widgets/audio_mute_status.dart';
 import 'package:hmssdk_flutter_example/common/peer_widgets/brb_tag.dart';
@@ -17,12 +14,9 @@ import 'package:hmssdk_flutter_example/common/peer_widgets/peer_name.dart';
 import 'package:hmssdk_flutter_example/common/peer_widgets/tile_border.dart';
 import 'package:hmssdk_flutter_example/common/util/app_color.dart';
 import 'package:hmssdk_flutter_example/common/util/utility_components.dart';
-import 'package:hmssdk_flutter_example/common/util/utility_function.dart';
 import 'package:provider/provider.dart';
 
 // Project imports
-
-import 'package:hmssdk_flutter_example/meeting/meeting_store.dart';
 import 'package:hmssdk_flutter_example/model/peer_track_node.dart';
 
 class PeerTile extends StatefulWidget {
@@ -50,17 +44,6 @@ class _PeerTileState extends State<PeerTile> {
 
   @override
   Widget build(BuildContext context) {
-    MeetingStore _meetingStore = context.read<MeetingStore>();
-
-    bool mutePermission =
-        _meetingStore.localPeer?.role.permissions.mute ?? false;
-    bool unMutePermission =
-        _meetingStore.localPeer?.role.permissions.unMute ?? false;
-    bool removePeerPermission =
-        _meetingStore.localPeer?.role.permissions.removeOthers ?? false;
-    bool changeRolePermission =
-        _meetingStore.localPeer?.role.permissions.changeRole ?? false;
-
     return Semantics(
       label: "fl_${context.read<PeerTrackNode>().peer.name}_video_tile",
       child: FocusDetector(
@@ -76,203 +59,77 @@ class _PeerTileState extends State<PeerTile> {
         },
         key: Key(context.read<PeerTrackNode>().uid),
         child: context.read<PeerTrackNode>().uid.contains("mainVideo")
-            ? InkWell(
-                onLongPress: () {
-                  if (!widget.islongPressEnabled) {
-                    return;
-                  }
-                  var peerTrackNode = context.read<PeerTrackNode>();
-                  HMSPeer peerNode = peerTrackNode.peer;
-                  if (peerTrackNode.peer.peerId !=
-                      _meetingStore.localPeer!.peerId)
-                    showDialog(
-                        context: context,
-                        builder: (_) => RemotePeerTileDialog(
-                              isAudioMuted:
-                                  peerTrackNode.audioTrack?.isMute ?? true,
-                              isVideoMuted: peerTrackNode.track == null
-                                  ? true
-                                  : peerTrackNode.track!.isMute,
-                              peerName: peerNode.name,
-                              changeVideoTrack: (mute, isVideoTrack) {
-                                Navigator.pop(context);
-                                _meetingStore.changeTrackState(
-                                    peerTrackNode.track!, mute);
-                              },
-                              changeAudioTrack: (mute, isAudioTrack) {
-                                Navigator.pop(context);
-                                _meetingStore.changeTrackState(
-                                    peerTrackNode.audioTrack!, mute);
-                              },
-                              removePeer: () async {
-                                Navigator.pop(context);
-                                var peer = await _meetingStore.getPeer(
-                                    peerId: peerNode.peerId);
-                                _meetingStore.removePeerFromRoom(peer!);
-                              },
-                              changeRole: () {
-                                Navigator.pop(context);
-                                showDialog(
-                                    context: context,
-                                    builder: (_) => ChangeRoleOptionDialog(
-                                          peerName: peerNode.name,
-                                          roles: _meetingStore.roles,
-                                          peer: peerNode,
-                                          changeRole: (role, forceChange) {
-                                            _meetingStore.changeRoleOfPeer(
-                                                peer: peerNode,
-                                                roleName: role,
-                                                forceChange: forceChange);
-                                          },
-                                        ));
-                              },
-                              changeLayer: () async {
-                                Navigator.pop(context);
-                                HMSRemoteVideoTrack track =
-                                    peerTrackNode.track as HMSRemoteVideoTrack;
-                                List<HMSSimulcastLayerDefinition>
-                                    layerDefinitions =
-                                    await track.getLayerDefinition();
-                                HMSSimulcastLayer selectedLayer =
-                                    await track.getLayer();
-                                if (layerDefinitions.isNotEmpty) {
-                                  showDialog(
-                                      context: context,
-                                      builder: (_) =>
-                                          ChangeSimulcastLayerOptionDialog(
-                                              layerDefinitions:
-                                                  layerDefinitions,
-                                              selectedLayer: selectedLayer,
-                                              track: track));
-                                } else {
-                                  Utilities.showToast(
-                                      "Simulcast not enabled for the role");
-                                }
-                              },
-                              mute: mutePermission,
-                              unMute: unMutePermission,
-                              removeOthers: removePeerPermission,
-                              roles: changeRolePermission,
-                              simulcast:
-                                  (!(peerTrackNode.track as HMSRemoteVideoTrack)
-                                      .isMute),
-                              pinTile: peerTrackNode.pinTile,
-                              changePinTileStatus: () {
-                                _meetingStore
-                                    .changePinTileStatus(peerTrackNode);
-                                Navigator.pop(context);
-                              },
-                            ));
-                  else
-                    showDialog(
-                        context: context,
-                        builder: (_) => LocalPeerTileDialog(
-                            isAudioMode: false,
-                            toggleCamera: () {
-                              if (_meetingStore.isVideoOn)
-                                _meetingStore.switchCamera();
-                            },
-                            peerName: peerNode.name,
-                            changeRole: () {
-                              Navigator.pop(context);
-                              showDialog(
-                                  context: context,
-                                  builder: (_) => ChangeRoleOptionDialog(
-                                        peerName: peerNode.name,
-                                        roles: _meetingStore.roles,
-                                        peer: peerNode,
-                                        changeRole: (role, forceChange) {
-                                          _meetingStore.changeRoleOfPeer(
-                                              peer: peerNode,
-                                              roleName: role,
-                                              forceChange: forceChange);
-                                        },
-                                      ));
-                            },
-                            roles: changeRolePermission,
-                            changeName: () async {
-                              String name =
-                                  await UtilityComponents.showInputDialog(
-                                      context: context,
-                                      placeholder: "Enter Name");
-                              if (name.isNotEmpty) {
-                                _meetingStore.changeName(name: name);
-                              }
-                            }));
-                },
-                child: Container(
-                  key: key,
-                  padding: EdgeInsets.all(2),
-                  margin: EdgeInsets.all(2),
-                  height: widget.itemHeight + 110,
-                  width: widget.itemWidth - 5.0,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: themeBottomSheetColor,
-                  ),
-                  child: Semantics(
-                    label:
-                        "fl_${context.read<PeerTrackNode>().peer.name}_video_on",
-                    child: Stack(
-                      children: [
-                        VideoView(
-                          uid: context.read<PeerTrackNode>().uid,
-                          scaleType: widget.scaleType,
+            ? Container(
+                key: key,
+                padding: EdgeInsets.all(2),
+                margin: EdgeInsets.all(2),
+                height: widget.itemHeight + 110,
+                width: widget.itemWidth - 5.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: themeBottomSheetColor,
+                ),
+                child: Semantics(
+                  label:
+                      "fl_${context.read<PeerTrackNode>().peer.name}_video_on",
+                  child: Stack(
+                    children: [
+                      VideoView(
+                        uid: context.read<PeerTrackNode>().uid,
+                        scaleType: widget.scaleType,
+                        itemHeight: widget.itemHeight,
+                        itemWidth: widget.itemWidth,
+                      ),
+                      TileBorder(
+                          itemHeight: widget.itemHeight,
+                          itemWidth: widget.itemWidth,
+                          name: context.read<PeerTrackNode>().peer.name,
+                          uid: context.read<PeerTrackNode>().uid),
+                      Semantics(
+                        label:
+                            "fl_${context.read<PeerTrackNode>().peer.name}_degraded_tile",
+                        child: DegradeTile(
                           itemHeight: widget.itemHeight,
                           itemWidth: widget.itemWidth,
                         ),
-
-                        Semantics(
-                          label:
-                              "fl_${context.read<PeerTrackNode>().peer.name}_degraded_tile",
-                          child: DegradeTile(
-                            itemHeight: widget.itemHeight,
-                            itemWidth: widget.itemWidth,
-                          ),
-                        ),
-                        if (!widget.isOneToOne)
-                          Positioned(
-                            //Bottom left
-                            bottom: 5,
-                            left: 5,
-                            child: Container(
-                              decoration: BoxDecoration(
-                                  color: themeTileNameColor,
-                                  borderRadius: BorderRadius.circular(8)),
-                              child: Center(
-                                child: Padding(
-                                  padding: const EdgeInsets.only(
-                                      left: 8.0, right: 4, top: 4, bottom: 4),
-                                  child: Row(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.center,
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      NetworkIconWidget(),
-                                      PeerName(),
-                                    ],
-                                  ),
+                      ),
+                      if (!widget.isOneToOne)
+                        Positioned(
+                          //Bottom left
+                          bottom: 5,
+                          left: 5,
+                          child: Container(
+                            decoration: BoxDecoration(
+                                color: themeTileNameColor,
+                                borderRadius: BorderRadius.circular(8)),
+                            child: Center(
+                              child: Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 8.0, right: 4, top: 4, bottom: 4),
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    NetworkIconWidget(),
+                                    PeerName(),
+                                  ],
                                 ),
                               ),
                             ),
                           ),
-                        HandRaise(), //top left
-                        BRBTag(), //bottom right
-                        AudioMuteStatus(), //top right
-                        if (!widget.isOneToOne)
-                          Semantics(
-                            label: "fl_stats_on_tile",
-                            child: RTCStatsView(
-                                isLocal:
-                                    context.read<PeerTrackNode>().peer.isLocal),
-                          ),
-                        TileBorder(
-                            itemHeight: widget.itemHeight,
-                            itemWidth: widget.itemWidth,
-                            name: context.read<PeerTrackNode>().peer.name,
-                            uid: context.read<PeerTrackNode>().uid)
-                      ],
-                    ),
+                        ),
+                      HandRaise(), //top left
+                      BRBTag(), //top left
+                      AudioMuteStatus(), //top right
+                      MoreOption(), //bottom right
+                      if (!widget.isOneToOne)
+                        Semantics(
+                          label: "fl_stats_on_tile",
+                          child: RTCStatsView(
+                              isLocal:
+                                  context.read<PeerTrackNode>().peer.isLocal),
+                        )
+                    ],
                   ),
                 ),
               )
