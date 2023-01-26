a76831211e4beb873891792ccf5e4f54908e8360
Decoder07 <pushpam@100ms.live>
2023-01-26 20:30:15 +0530

    Optimized HMSVideoView

--- android/src/main/kotlin/live/hms/hmssdk_flutter/views/HMSVideoView.kt
+++ android/src/main/kotlin/live/hms/hmssdk_flutter/views/HMSVideoView.kt
@@ -8,6 +8,7 @@ import android.graphics.Bitmap
 import android.util.Base64
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.FrameLayout
 import live.hms.hmssdk_flutter.HmssdkFlutterPlugin
 import live.hms.hmssdk_flutter.R
@@ -18,13 +19,14 @@ import java.io.ByteArrayOutputStream
 
 class HMSVideoView(
     context: Context,
-    setMirror: Boolean,
-    scaleType: Int? = RendererCommon.ScalingType.SCALE_ASPECT_FIT.ordinal,
+    private val setMirror: Boolean,
+    private val scaleType: Int? = RendererCommon.ScalingType.SCALE_ASPECT_FIT.ordinal,
     private val track: HMSVideoTrack?,
-    disableAutoSimulcastLayerSelect: Boolean
+    private val disableAutoSimulcastLayerSelect: Boolean
 ) : FrameLayout(context, null) {
 
-    private val hmsVideoView: HMSVideoView
+    private var hmsVideoView: HMSVideoView? = null
+    private var view:View? = null
     private val broadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(contxt: Context?, intent: Intent?) {
             if (intent?.action == track?.trackId) {
@@ -39,58 +41,79 @@ class HMSVideoView(
         }
     }
     init {
-        val inflater =
-            getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        val view = inflater.inflate(R.layout.hms_video_view, this)
-        hmsVideoView = view.findViewById(R.id.hmsVideoView)
-        hmsVideoView.setEnableHardwareScaler(false)
-        hmsVideoView.setMirror(setMirror)
-        hmsVideoView.disableAutoSimulcastLayerSelect(disableAutoSimulcastLayerSelect)
-        if ((scaleType ?: 0) <= RendererCommon.ScalingType.values().size) {
-            hmsVideoView.setScalingType(RendererCommon.ScalingType.values()[scaleType ?: 0])
+        view =
+            (context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater).inflate(R.layout.hms_video_view, this)
+        if(view != null){
+            hmsVideoView = view?.findViewById(R.id.hmsVideoView)
+        }
+        else{
+            Log.i("HMSVideoView Error","HMSVideoView init error view is null")
         }
     }
 
     private fun captureSnapshot() {
         var byteArray: ByteArray?
-        hmsVideoView.captureBitmap({ bitmap ->
-            if (bitmap != null) {
-                val stream = ByteArrayOutputStream()
-                bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
-                byteArray = stream.toByteArray()
-                bitmap.recycle()
-                val data = Base64.encodeToString(byteArray, Base64.DEFAULT)
-                if (HmssdkFlutterPlugin.hmssdkFlutterPlugin != null) {
-                    if (HmssdkFlutterPlugin.hmssdkFlutterPlugin?.hmsVideoViewResult != null) {
-                        HmssdkFlutterPlugin.hmssdkFlutterPlugin?.hmsVideoViewResult?.success(data)
+        if(hmsVideoView != null){
+            hmsVideoView?.captureBitmap({ bitmap ->
+                if (bitmap != null) {
+                    val stream = ByteArrayOutputStream()
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
+                    byteArray = stream.toByteArray()
+                    bitmap.recycle()
+                    val data = Base64.encodeToString(byteArray, Base64.DEFAULT)
+                    if (HmssdkFlutterPlugin.hmssdkFlutterPlugin != null) {
+                        if (HmssdkFlutterPlugin.hmssdkFlutterPlugin?.hmsVideoViewResult != null) {
+                            HmssdkFlutterPlugin.hmssdkFlutterPlugin?.hmsVideoViewResult?.success(data)
+                        } else {
+                            Log.i("Receiver error", "hmsVideoViewResult is null")
+                        }
                     } else {
-                        Log.i("Receiver error", "hmsVideoViewResult is null")
+                        Log.i("Receiver error", "hmssdkFlutterPlugin is null")
                     }
-                } else {
-                    Log.i("Receiver error", "hmssdkFlutterPlugin is null")
                 }
-            }
-        })
+            })
+        }
+        else{
+            Log.i("Receiver error", "hmsVideoView is null")
+        }
     }
 
     fun onDisposeCalled() {
-        super.onDetachedFromWindow()
-        hmsVideoView.removeTrack()
+        if(hmsVideoView != null){
+            hmsVideoView?.removeTrack()
+        }
+        else{
+            Log.i("HMSVideoView error","onDisposeCalled error hmsVideoView is null")
+        }
+        this.removeView(view)
+        view = null
+        hmsVideoView = null
         context.unregisterReceiver(broadcastReceiver)
     }
 
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
         if (track != null) {
-            hmsVideoView.addTrack(track)
+            hmsVideoView?.setEnableHardwareScaler(false)
+            hmsVideoView?.setMirror(setMirror)
+            hmsVideoView?.disableAutoSimulcastLayerSelect(disableAutoSimulcastLayerSelect)
+            if ((scaleType ?: 0) <= RendererCommon.ScalingType.values().size) {
+                hmsVideoView?.setScalingType(RendererCommon.ScalingType.values()[scaleType ?: 0])
+            }
+            hmsVideoView?.addTrack(track)
             context.registerReceiver(broadcastReceiver, IntentFilter(track.trackId))
         } else {
-            Log.e("HMSVideoView Error", "track is null, cannot attach null track")
+            Log.e("HMSVideoView Error", "onAttachedToWindow error track is null, cannot attach null track")
         }
     }
 
     override fun onDetachedFromWindow() {
         super.onDetachedFromWindow()
-        hmsVideoView.removeTrack()
+        if(hmsVideoView != null){
+            hmsVideoView?.removeTrack()
+        }
+        else{
+            Log.i("HMSVideoView error","onDetachedFromWindow error hmsVideoView is null")
+        }
     }
 }
