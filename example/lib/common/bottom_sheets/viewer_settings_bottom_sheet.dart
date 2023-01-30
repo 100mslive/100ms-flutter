46228b50f01778614f3bc197bd66cbb2b2240538
Pushpam <93931528+Decoder07@users.noreply.github.com>
2023-01-30 20:41:13 +0530

    Fixed HLS UI (#1118)

--- example/lib/common/bottom_sheets/viewer_settings_bottom_sheet.dart
+++ example/lib/common/bottom_sheets/viewer_settings_bottom_sheet.dart
@@ -7,6 +7,9 @@ import 'package:hmssdk_flutter_example/common/util/utility_components.dart';
 import 'package:hmssdk_flutter_example/common/bottom_sheets/participants_bottom_sheet.dart';
 import 'package:hmssdk_flutter_example/meeting/meeting_store.dart';
 import 'package:provider/provider.dart';
+import 'package:badges/badges.dart' as badge;
+
+import 'notification_settings_bottom_sheet.dart';
 
 class ViewerSettingsBottomSheet extends StatefulWidget {
   @override
@@ -17,7 +20,6 @@ class ViewerSettingsBottomSheet extends StatefulWidget {
 class _ViewerSettingsBottomSheetState extends State<ViewerSettingsBottomSheet> {
   @override
   Widget build(BuildContext context) {
-    MeetingStore _meetingStore = context.read<MeetingStore>();
     return FractionallySizedBox(
       heightFactor: 0.5,
       child: Padding(
@@ -64,6 +66,126 @@ class _ViewerSettingsBottomSheetState extends State<ViewerSettingsBottomSheet> {
                   height: 5,
                 ),
               ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  InkWell(
+                    onTap: () async {
+                      Navigator.pop(context);
+                      showModalBottomSheet(
+                        isScrollControlled: true,
+                        backgroundColor: themeBottomSheetColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        context: context,
+                        builder: (ctx) => ChangeNotifierProvider.value(
+                            value: context.read<MeetingStore>(),
+                            child: ParticipantsBottomSheet()),
+                      );
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: moreSettingsButtonColor,
+                          borderRadius: BorderRadius.circular(10)),
+                      height: 100,
+                      width: 150,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          badge.Badge(
+                            badgeStyle: badge.BadgeStyle(
+                                badgeColor: hmsdefaultColor,
+                                padding: EdgeInsets.all(
+                                    context.read<MeetingStore>().peers.length <
+                                            1000
+                                        ? 5
+                                        :8)),
+                            badgeContent: Text(context
+                                    .read<MeetingStore>()
+                                    .peers
+                                    .length
+                                    .toString()),
+                            child: Padding(
+                              padding: EdgeInsets.all((context.read<MeetingStore>().peers.length <
+                                            1000
+                                        ? 5
+                                        :10)),
+                              child: SvgPicture.asset(
+                                "assets/icons/participants.svg",
+                                color: themeDefaultColor,
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            "Participants",
+                            semanticsLabel: "participants_button",
+                            style: GoogleFonts.inter(
+                                fontSize: 14,
+                                color: themeDefaultColor,
+                                letterSpacing: 0.25,
+                                fontWeight: FontWeight.w600),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () async {
+                      Navigator.pop(context);
+                      showDialog(
+                          context: context,
+                          builder: (_) => AspectRatioOptionDialog(
+                                  availableAspectRatios: [
+                                    "16:9",
+                                    "4:3",
+                                    "1:1",
+                                    "3:4",
+                                    "9:16"
+                                  ],
+                                  setAspectRatio: (double aspectRatio) {
+                                    context
+                                        .read<MeetingStore>()
+                                        .setPIPVideoController(true,
+                                            aspectRatio: aspectRatio);
+                                    Navigator.pop(context);
+                                  }));
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: moreSettingsButtonColor,
+                          borderRadius: BorderRadius.circular(10)),
+                      height: 100,
+                      width: 150,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          SvgPicture.asset(
+                            "assets/icons/aspect_ratio.svg",
+                            color: themeDefaultColor,
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            "Set Aspect Ratio",
+                            semanticsLabel: "fl_aspect_ratio",
+                            style: GoogleFonts.inter(
+                                fontSize: 14,
+                                color: themeDefaultColor,
+                                letterSpacing: 0.25,
+                                fontWeight: FontWeight.w600),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
               ListTile(
                 horizontalTitleGap: 2,
                 onTap: () async {
@@ -93,88 +215,48 @@ class _ViewerSettingsBottomSheetState extends State<ViewerSettingsBottomSheet> {
                 ),
               ),
               ListTile(
-                horizontalTitleGap: 2,
-                onTap: () {
-                  showModalBottomSheet(
-                    isScrollControlled: true,
-                    backgroundColor: themeBottomSheetColor,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    context: context,
-                    builder: (ctx) => ChangeNotifierProvider.value(
-                        value: _meetingStore, child: ParticipantsBottomSheet()),
-                  );
-                },
-                contentPadding: EdgeInsets.zero,
-                leading: SvgPicture.asset(
-                  "assets/icons/participants.svg",
-                  fit: BoxFit.scaleDown,
-                ),
-                title: Text(
-                  "Participants",
-                  style: GoogleFonts.inter(
-                      fontSize: 14,
-                      color: themeDefaultColor,
-                      letterSpacing: 0.25,
-                      fontWeight: FontWeight.w600),
-                ),
-              ),
-              ListTile(
                   horizontalTitleGap: 2,
                   onTap: () async {
-                    showDialog(
-                        context: context,
-                        builder: (_) => AspectRatioOptionDialog(
-                                availableAspectRatios: [
-                                  "16:9",
-                                  "4:3",
-                                  "1:1",
-                                  "3:4",
-                                  "9:16"
-                                ],
-                                setAspectRatio: (double aspectRatio) {
-                                  context
-                                      .read<MeetingStore>()
-                                      .setPIPVideoController(true,
-                                          aspectRatio: aspectRatio);
-                                  Navigator.pop(context);
-                                }));
+                    context.read<MeetingStore>().changeMetadata();
+                    Navigator.pop(context);
                   },
                   contentPadding: EdgeInsets.zero,
                   leading: SvgPicture.asset(
-                    "assets/icons/aspect_ratio.svg",
+                    "assets/icons/hand_outline.svg",
                     fit: BoxFit.scaleDown,
-                    color: themeDefaultColor,
+                    color: context.read<MeetingStore>().isRaisedHand?errorColor:themeDefaultColor,
                   ),
                   title: Text(
-                    "Set Aspect Ratio",
-                    semanticsLabel: "fl_aspect_ratio",
+                    "Raise Hand",
+                    semanticsLabel: "hand_raise_button",
                     style: GoogleFonts.inter(
                         fontSize: 14,
-                        color: themeDefaultColor,
+                        color: context.read<MeetingStore>().isRaisedHand?errorColor:themeDefaultColor,
                         letterSpacing: 0.25,
                         fontWeight: FontWeight.w600),
                   )),
               ListTile(
                   horizontalTitleGap: 2,
                   onTap: () async {
-                    context
-                        .read<MeetingStore>()
-                        .hlsVideoController
-                        ?.enablePictureInPicture(
-                            context.read<MeetingStore>().pipFlutterPlayerKey);
                     Navigator.pop(context);
+                    showModalBottomSheet(
+                        isScrollControlled: true,
+                        backgroundColor: themeBottomSheetColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        context: context,
+                        builder: (ctx) => NotificationSettingsBottomSheet());
                   },
                   contentPadding: EdgeInsets.zero,
                   leading: SvgPicture.asset(
-                    "assets/icons/screen_share.svg",
+                    "assets/icons/notification.svg",
                     fit: BoxFit.scaleDown,
                     color: themeDefaultColor,
                   ),
                   title: Text(
-                    "Enter Pip Mode",
-                    semanticsLabel: "fl_pip_mode",
+                    "Modify Notifications",
+                    semanticsLabel: "fl_notification_setting",
                     style: GoogleFonts.inter(
                         fontSize: 14,
                         color: themeDefaultColor,
