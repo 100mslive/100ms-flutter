b2e9404c20cfaebfe2331355fbc0d8343caebd4b
Decoder07 <pushpam@100ms.live>
2023-03-13 17:33:16 +0530

    Updated flavor names

--- example/lib/service/constant.dart
+++ example/lib/service/constant.dart
@@ -1,4 +1,6 @@
 //Class contains the constants used throughout the application
+import 'package:hmssdk_flutter_example/common/util/utility_function.dart';
+
 class Constant {
   static String prodTokenEndpoint =
       "https://prod-in.100ms.live/hmsapi/get-token";
@@ -21,4 +23,5 @@ class Constant {
   static String meetingUrl = defaultMeetingLink;
   static String meetingCode = "";
   static String streamingUrl = "";
+  static AppFlavors appFlavor = AppFlavors.hmsInternal;
 }
