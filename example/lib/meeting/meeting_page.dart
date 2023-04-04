1d5597c72c309da403e4ad8443f89ff010da419a
Decoder07 <pushpam@100ms.live>
2023-04-04 13:21:39 +0530

    Added backward compatibility

--- example/lib/meeting/meeting_page.dart
+++ example/lib/meeting/meeting_page.dart
@@ -205,7 +205,8 @@ class _MeetingPageState extends State<MeetingPage> {
                 if (data.item2 != null) {
                   if (data.item2?.code?.errorCode == 1003 ||
                       data.item2?.code?.errorCode == 2000 ||
-                      data.item2?.code?.errorCode == 4005) {
+                      data.item2?.code?.errorCode == 4005 ||
+                      data.item2?.code?.errorCode == 424) {
                     WidgetsBinding.instance.addPostFrameCallback((_) {
                       UtilityComponents.showErrorDialog(
                           context: context,
