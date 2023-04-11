d4c16bcaa24289d399f1123e39587c3f6cbc8768
Decoder07 <pushpam@100ms.live>
2023-04-11 12:54:07 +0530

    Added exception logs

--- lib/src/model/hms_android_pip_controller.dart
+++ lib/src/model/hms_android_pip_controller.dart
@@ -1,4 +1,5 @@
 import 'package:hmssdk_flutter/hmssdk_flutter.dart';
+import 'package:hmssdk_flutter/src/enum/hms_logs_update_listener.dart';
 import 'package:hmssdk_flutter/src/service/platform_service.dart';
 
 /// [HMSAndroidPIPController] is used to setup and start the PIP in android. To know more visit [here](https://www.100ms.live/docs/flutter/v2/advanced-features/pip-mode).
@@ -16,30 +17,54 @@ class HMSAndroidPIPController {
   ///`Note: Minimum version required to support PiP is Android 8.0 (API level 26)`
   static Future<bool> start(
       {List<int>? aspectRatio, bool? autoEnterPip}) async {
-    final bool? result = await PlatformService.invokeMethod(
-        PlatformMethod.enterPipMode,
-        arguments: {
-          "aspect_ratio": aspectRatio ?? [16, 9],
-          "auto_enter_pip": autoEnterPip ?? true
-        });
-    return result ?? false;
+    try {
+      final bool? result = await PlatformService.invokeMethod(
+          PlatformMethod.enterPipMode,
+          arguments: {
+            "aspect_ratio": aspectRatio ?? [16, 9],
+            "auto_enter_pip": autoEnterPip ?? true
+          });
+      return result ?? false;
+    } catch (e) {
+      PlatformService.notifyLogsUpdateListeners(
+          HMSLogsUpdateListenerMethod.onLogsUpdate, [
+        "HMSException occured in HMSAndroidPIPController start exception: $e"
+      ]);
+      return false;
+    }
   }
 
   ///Method to check whether pip mode is active currently
   ///
   ///Refer [PIP mode guide here](https://www.100ms.live/docs/flutter/v2/advanced-features/pip-mode)
   static Future<bool> isActive() async {
-    final bool? result =
-        await PlatformService.invokeMethod(PlatformMethod.isPipActive);
-    return result ?? false;
+    try {
+      final bool? result =
+          await PlatformService.invokeMethod(PlatformMethod.isPipActive);
+      return result ?? false;
+    } catch (e) {
+      PlatformService.notifyLogsUpdateListeners(
+          HMSLogsUpdateListenerMethod.onLogsUpdate, [
+        "HMSException occured in HMSAndroidPIPController isActive exception: $e"
+      ]);
+      return false;
+    }
   }
 
   ///Method to check whether pip mode is available for the current device
   ///
   ///Refer [PIP mode guide here](https://www.100ms.live/docs/flutter/v2/advanced-features/pip-mode)
   static Future<bool> isAvailable() async {
-    final bool? result =
-        await PlatformService.invokeMethod(PlatformMethod.isPipAvailable);
-    return result ?? false;
+    try {
+      final bool? result =
+          await PlatformService.invokeMethod(PlatformMethod.isPipAvailable);
+      return result ?? false;
+    } catch (e) {
+      PlatformService.notifyLogsUpdateListeners(
+          HMSLogsUpdateListenerMethod.onLogsUpdate, [
+        "HMSException occured in HMSAndroidPIPController isAvailable exception: $e"
+      ]);
+      return false;
+    }
   }
 }
