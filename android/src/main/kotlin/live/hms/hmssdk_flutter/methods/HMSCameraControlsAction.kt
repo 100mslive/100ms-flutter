b28d0aba91260095ec38c6c0a1811fb83d86cfea
Decoder07 <pushpam@100ms.live>
2023-04-14 12:26:39 +0530

    Added captureImage method

--- android/src/main/kotlin/live/hms/hmssdk_flutter/methods/HMSCameraControlsAction.kt
+++ android/src/main/kotlin/live/hms/hmssdk_flutter/methods/HMSCameraControlsAction.kt
@@ -2,6 +2,7 @@ package live.hms.hmssdk_flutter.methods
 
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel.Result
+import live.hms.hmssdk_flutter.HMSResultExtension
 import live.hms.video.media.capturers.camera.CameraControl
 import live.hms.video.media.tracks.HMSLocalVideoTrack
 import live.hms.video.sdk.HMSSDK
@@ -13,6 +14,7 @@ class HMSCameraControlsAction {
             when(call.method){
                 "is_tap_to_focus_supported" -> isTapToFocusSupported(result,hmssdk)
                 "is_zoom_supported" -> isZoomSupported(result,hmssdk)
+                "capture_image_at_max_supported_resolution" -> captureImageAtMaxSupportedResolution(call,result,hmssdk)
                 else -> {
                     result.notImplemented()
                 }
@@ -23,7 +25,7 @@ class HMSCameraControlsAction {
             hmssdk.getLocalPeer()?.let { localPeer ->
                 localPeer.videoTrack?.let { localVideoTrack ->
                     localVideoTrack.getCameraControl()?.let { cameraControl ->
-                        result.success(cameraControl.isTapToFocusSupported())
+                        HMSResultExtension.toDictionary(true,cameraControl.isTapToFocusSupported())
                         return
                     }
                 }
@@ -31,16 +33,32 @@ class HMSCameraControlsAction {
             result.success(false)
         }
 
-        private  fun isZoomSupported(result: Result,hmssdk: HMSSDK){
+        private fun isZoomSupported(result: Result,hmssdk: HMSSDK){
             hmssdk.getLocalPeer()?.let { localPeer ->
                 localPeer.videoTrack?.let { localVideoTrack ->
                     localVideoTrack.getCameraControl()?.let { cameraControl ->
-                        result.success(cameraControl.isZoomSupported())
+                        HMSResultExtension.toDictionary(true,cameraControl.isZoomSupported())
                         return
                     }
                 }
             }
             result.success(false)
+        }
+
+        private fun captureImageAtMaxSupportedResolution(call: MethodCall,result: Result,hmssdk: HMSSDK){
+
+            val filePath = call.argument<String>("file_path")?: run {
+
+            }
+
+            hmssdk.getLocalPeer()?.let { localPeer ->
+                localPeer.videoTrack?.let { localVideoTrack ->
+                    localVideoTrack.getCameraControl()?.let { cameraControl ->
+                        cameraControl.captureImageAtMaxSupportedResolution()
+                        return
+                    }
+                }
+            }
         }
 
     }
