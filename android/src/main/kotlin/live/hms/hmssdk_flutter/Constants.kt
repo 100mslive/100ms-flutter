ec6797b3581d18b945f0eae6edc2f69df0e99c10
Decoder07 <pushpam@100ms.live>
2023-04-21 13:00:57 +0530

    Added constant intents for audio/screen share

--- android/src/main/kotlin/live/hms/hmssdk_flutter/Constants.kt
+++ android/src/main/kotlin/live/hms/hmssdk_flutter/Constants.kt
@@ -5,6 +5,14 @@ class Constants {
         const val SCREEN_SHARE_INTENT_REQUEST_CODE = 192
 
         const val AUDIO_SHARE_INTENT_REQUEST_CODE = 182
+
+        const val HMSSDK_RECEIVER = "ACTIVITY_RECEIVER"
+
+        const val METHOD_CALL = "method_name"
+
+        const val SCREEN_SHARE_REQUEST = "REQUEST_SCREEN_SHARE"
+
+        const val AUDIO_SHARE_REQUEST = "REQUEST_AUDIO_SHARE"
     }
 
 
