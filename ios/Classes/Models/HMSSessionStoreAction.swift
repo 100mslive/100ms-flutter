6f692fce203bc7303dfe92f9aeefba71fc36c213
Yogesh Singh <yogesh@100ms.live>
2023-04-26 16:40:27 +0530

    added func to get object for key from session store

--- ios/Classes/Models/HMSSessionStoreAction.swift
+++ ios/Classes/Models/HMSSessionStoreAction.swift
@@ -15,22 +15,48 @@ class HMSSessionStoreAction {
         switch call.method {
         case "get_session_metadata_for_key":
             getSessionMetadata(call, result, plugin)
-            
+
         case "set_session_metadata_for_key":
             setSessionMetadata(call, result, plugin)
-            
+
         default:
             result(FlutterMethodNotImplemented)
         }
     }
-    
-    
+
     static private func getSessionMetadata(_ call: FlutterMethodCall, _ result: @escaping FlutterResult, _ plugin: SwiftHmssdkFlutterPlugin) {
-        
+
+        guard let store = plugin.sessionStore
+        else {
+            result(HMSResultExtension.toDictionary(false, HMSErrorExtension.getError("\(#function) Session Store is null.")))
+            return
+        }
+
+        guard let arguments = call.arguments as? [AnyHashable: Any],
+            let key = arguments["key"] as? String
+        else {
+            result(HMSResultExtension.toDictionary(false, HMSErrorExtension.getError("\(#function) Key to be fetched from Session Store is null.")))
+            return
+        }
+
+        store.object(forKey: key) { value, error in
+
+            if let error = error {
+                result(HMSResultExtension.toDictionary(false, HMSErrorExtension.getError("\(#function) Error in fetching key: \(key) from Session Store. Error: \(error.localizedDescription)")))
+            }
+
+            guard let value = value
+            else {
+                result(HMSResultExtension.toDictionary(false, HMSErrorExtension.getError("\(#function) Error in fetching key: \(key) from Session Store due to an unknown error")))
+                return
+            }
+
+            result(HMSResultExtension.toDictionary(true, value))
+        }
+
     }
-    
-    
+
     static private func setSessionMetadata(_ call: FlutterMethodCall, _ result: @escaping FlutterResult, _ plugin: SwiftHmssdkFlutterPlugin) {
-        
+
     }
 }
