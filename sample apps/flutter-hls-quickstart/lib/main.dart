f4f17efc92ec3c183abca3b0319b4dc4e41257e0
Yogesh Singh <yogesh@100ms.live>
2023-05-02 23:36:41 +0530

    updated default Auth Token value in HLS Example app to avoid dart lint from failing

--- sample apps/flutter-hls-quickstart/lib/main.dart
+++ sample apps/flutter-hls-quickstart/lib/main.dart
@@ -81,8 +81,8 @@ class _MyHomePageState extends State<MyHomePage> {
                       context,
                       CupertinoPageRoute(
                           builder: (_) => MeetingPage(
-                              authToken:
-                                  /*
+                              authToken: "ENTER YOUR AUTH TOKEN HERE",
+                              /*
                                   * Paste Auth Token for your Room from 100ms Dashboard here
                                   * https://dashboard.100ms.live/
                                   */
@@ -113,8 +113,8 @@ class _MyHomePageState extends State<MyHomePage> {
                       context,
                       CupertinoPageRoute(
                           builder: (_) => HLSViewerPage(
-                              authToken:
-                                  /*
+                              authToken: "ENTER YOUR AUTH TOKEN HERE",
+                              /*
                                   * Paste Auth Token for your Room from 100ms Dashboard here
                                   * https://dashboard.100ms.live/
                                   */
