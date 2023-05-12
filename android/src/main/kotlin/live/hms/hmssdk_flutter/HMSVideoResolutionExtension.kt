54e3495be45a0d1a3e7a44681731e78034950b3a
Yogesh Singh <yogesh@100ms.live>
2023-05-12 16:39:22 +0530

    corrected SDK lint warnings in Kotlin plugin

--- android/src/main/kotlin/live/hms/hmssdk_flutter/HMSVideoResolutionExtension.kt
+++ android/src/main/kotlin/live/hms/hmssdk_flutter/HMSVideoResolutionExtension.kt
@@ -4,16 +4,15 @@ import live.hms.video.media.settings.HMSVideoResolution
 
 class HMSVideoResolutionExtension {
 
-    companion object{
+    companion object {
 
-        fun toDictionary(hmsVideoResolution: HMSVideoResolution?):HashMap<String,Any?>?{
+        fun toDictionary(hmsVideoResolution: HMSVideoResolution?): HashMap<String, Any?>? {
+            val args = HashMap<String, Any?>()
+            if (hmsVideoResolution == null) return null
+            args["height"] = hmsVideoResolution.height / 1.0
+            args["width"] = hmsVideoResolution.width / 1.0
 
-            val args=HashMap<String,Any?>()
-            if(hmsVideoResolution == null) return null
-            args["height"] = hmsVideoResolution.height/1.0
-            args["width"] = hmsVideoResolution.width/1.0
-
-            return args;
+            return args
         }
     }
 }
