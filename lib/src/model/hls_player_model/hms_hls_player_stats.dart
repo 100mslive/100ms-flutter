3ec8fbb48fc92008f8d1553230e388b5b37008d1
Yogesh Singh <yogesh@100ms.live>
2023-05-25 16:58:00 +0530

    fixed dart warnings

--- lib/src/model/hls_player_model/hms_hls_player_stats.dart
+++ lib/src/model/hls_player_model/hms_hls_player_stats.dart
@@ -1,5 +1,4 @@
 class HMSHLSPlayerStats {
-
   final double bandWidthEstimate;
   final int totalBytesLoaded;
   final double bufferedDuration;
@@ -21,15 +20,25 @@ class HMSHLSPlayerStats {
 
   factory HMSHLSPlayerStats.fromMap(Map map) {
     return HMSHLSPlayerStats(
-        bandWidthEstimate: (map["bandwidth_estimate"] is int)?map["bandwidth_estimate"].toDouble():map["bandwidth_estimate"],
+        bandWidthEstimate: (map["bandwidth_estimate"] is int)
+            ? map["bandwidth_estimate"].toDouble()
+            : map["bandwidth_estimate"],
         totalBytesLoaded: map["total_bytes_loaded"],
-        bufferedDuration: (map["buffered_duration"] is int)?map["buffered_duration"].toDouble():map["buffered_duration"],
-        distanceFromLive: (map["distance_from_live"] is int)?map["distance_from_live"].toDouble():map["distance_from_live"],
+        bufferedDuration: (map["buffered_duration"] is int)
+            ? map["buffered_duration"].toDouble()
+            : map["buffered_duration"],
+        distanceFromLive: (map["distance_from_live"] is int)
+            ? map["distance_from_live"].toDouble()
+            : map["distance_from_live"],
         droppedFrameCount: map["dropped_frame_count"],
-        averageBitrate: (map["average_bitrate"] is int)?map["average_bitrate"].toDouble():map["average_bitrate"],
-        videoHeight: (map["video_height"] is int)?map["video_height"].toDouble():map["video_height"] ,
-        videoWidth: (map["video_width"] is int)?map["video_width"].toDouble():map["video_width"]
-        );
+        averageBitrate: (map["average_bitrate"] is int)
+            ? map["average_bitrate"].toDouble()
+            : map["average_bitrate"],
+        videoHeight: (map["video_height"] is int)
+            ? map["video_height"].toDouble()
+            : map["video_height"],
+        videoWidth: (map["video_width"] is int)
+            ? map["video_width"].toDouble()
+            : map["video_width"]);
   }
 }
-
