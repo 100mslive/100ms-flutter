4351676694ac2bf7e53edd28fd05809f06d091b2
Decoder07 <pushpam@100ms.live>
2023-06-19 16:46:36 +0530

    Added methods to enable the always ON Screen capabilities

--- ios/Classes/SwiftHmssdkFlutterPlugin.swift
+++ ios/Classes/SwiftHmssdkFlutterPlugin.swift
@@ -282,6 +282,9 @@ public class SwiftHmssdkFlutterPlugin: NSObject, FlutterPlugin, HMSUpdateListene
         case "start_hls_player", "stop_hls_player", "pause_hls_player", "resume_hls_player", "seek_to_live_position", "seek_forward", "seek_backward", "set_hls_player_volume", "add_hls_stats_listener", "remove_hls_stats_listener":
             HMSHLSPlayerAction.hlsPlayerAction(call, result)
 
+        case "toggle_always_screen_on":
+            toggleAlwaysScreenOn(result)
+
         default:
             result(FlutterMethodNotImplemented)
         }
@@ -765,6 +768,22 @@ public class SwiftHmssdkFlutterPlugin: NSObject, FlutterPlugin, HMSUpdateListene
 
     private func destroy(_ result: @escaping FlutterResult) {
         hmsSDK = nil
+        result(nil)
+    }
+    
+    /**
+                [toggleAlwaysScreenOn] provides a way to keep the screen always ON
+                when enabled.
+     */
+    private func toggleAlwaysScreenOn(_ result: @escaping FlutterResult){
+        
+        if(UIApplication.shared.isIdleTimerDisabled){
+            UIApplication.shared.isIdleTimerDisabled = false
+        }
+        else{
+            UIApplication.shared.isIdleTimerDisabled = true
+        }
+        result(nil)
     }
 
     // MARK: - Role based Actions
--- lib/src/hmssdk.dart
+++ lib/src/hmssdk.dart
@@ -1281,6 +1281,11 @@ class HMSSDK {
     PlatformService.invokeMethod(PlatformMethod.destroy);
   }
 
+  ///Method to toggle the always screen on capabilities(similar to wakelock)
+  void toggleAlwaysScreenOn() {
+    PlatformService.invokeMethod(PlatformMethod.toggleAlwaysScreenOn);
+  }
+
   ///Method to activate pipMode in the application
   ///
   ///**Parameters**:
