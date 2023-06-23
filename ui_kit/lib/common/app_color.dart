2ec83a6875b46381384fc46c580974b8e6f2ea99
Decoder07 <pushpam@100ms.live>
2023-06-23 15:31:39 +0530

    Added colors

--- ui_kit/lib/common/app_color.dart
+++ ui_kit/lib/common/app_color.dart
@@ -63,3 +63,63 @@ Color hmsdefaultColor = const Color.fromRGBO(36, 113, 237, 1);
 Color popupButtonBorderColor = const Color.fromRGBO(107, 125, 153, 1);
 Color dialogcontentColor = const Color.fromRGBO(145, 127, 129, 1);
 Color moreSettingsButtonColor = const Color.fromRGBO(30, 35, 42, 1);
+
+int _getColorFromHex(String hexColor) {
+  hexColor = hexColor.toUpperCase().replaceAll("#", "");
+  if (hexColor.length == 6) {
+    hexColor = "FF" + hexColor;
+  }
+
+  final hexNum = int.parse(hexColor, radix: 16);
+
+  if (hexNum == 0) {
+    return 0xff000000;
+  }
+
+  return hexNum;
+}
+
+Color primaryDefault = Color(_getColorFromHex("#2572ED"));
+Color primaryBright = Color(_getColorFromHex("#538DFF"));
+Color primaryDim = Color(_getColorFromHex("#002D6D"));
+Color primaryDisabled = Color(_getColorFromHex("#004299"));
+
+Color onPrimaryHighEmphasis = Color(_getColorFromHex("#FFFFFF"));
+Color onPrimaryMediumEmphasis = Color(_getColorFromHex("#CCDAFF"));
+Color onPrimaryLowEmphasis = Color(_getColorFromHex("#84AAFF"));
+
+Color secondaryDefault = Color(_getColorFromHex("#444954"));
+Color secondaryBright = Color(_getColorFromHex("#70778B"));
+Color secondaryDim = Color(_getColorFromHex("#293042"));
+Color secondaryDisabled = Color(_getColorFromHex("#404759"));
+
+Color onSecondaryHighEmphasis = Color(_getColorFromHex("#FFFFFF"));
+Color onSecondaryMediumEmphasis = Color(_getColorFromHex("#D3D9F0"));
+Color onSecondaryLowEmphasis = Color(_getColorFromHex("#A4ABC0"));
+
+Color backgroundDefault = Color(_getColorFromHex("#0B0E15"));
+Color backgroundDim = Color(_getColorFromHex("#000000"));
+
+Color surfaceDefault = Color(_getColorFromHex("#191B23"));
+Color surfaceBright = Color(_getColorFromHex("#272A31"));
+Color surfaceBrighter = Color(_getColorFromHex("#2E3038"));
+Color surfaceDim = Color(_getColorFromHex("#11131A"));
+
+Color onSurfaceHighEmphasis = Color(_getColorFromHex("#EFF0FA"));
+Color onSurfaceMediumEmphasis = Color(_getColorFromHex("#C5C6D0"));
+Color onSurfaceLowEmphasis = Color(_getColorFromHex("#8F9099"));
+
+Color alertErrorDefault = Color(_getColorFromHex("#C74E5B"));
+Color alertErrorBright = Color(_getColorFromHex("#FFB2B6"));
+Color alertErrorBrighter = Color(_getColorFromHex("#FFEDEC"));
+Color alertErrorDim = Color(_getColorFromHex("#270005"));
+
+Color alertSuccess = Color(_getColorFromHex("#36B37E"));
+Color alertWarning = Color(_getColorFromHex("#FFAB00"));
+
+Color borderDefault = Color(_getColorFromHex("#1D1F27"));
+Color borderBright = Color(_getColorFromHex("#272A31"));
+Color borderPrimary = Color(_getColorFromHex("#2572ED"));
+
+Color baseBlack = Color(_getColorFromHex("#000000"));
+Color baseWhite = Color(_getColorFromHex("#FFFFFF"));
