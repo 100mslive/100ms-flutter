75979ef81c80371d7c7267b536de40c4cbfeba61
Yogesh Singh <yogesh@100ms.live>
2023-06-30 21:08:45 +0530

    refactored applying of audio & video track settings

--- android/src/main/kotlin/live/hms/hmssdk_flutter/HMSTrackSettingsExtension.kt
+++ android/src/main/kotlin/live/hms/hmssdk_flutter/HMSTrackSettingsExtension.kt
@@ -9,75 +9,85 @@ import live.hms.video.sdk.HMSSDK
 class HMSTrackSettingsExtension {
 
     companion object {
-        fun toDictionary(hmssdk: HMSSDK): HashMap<String, Any>? {
+        fun toDictionary(hmssdk: HMSSDK): HashMap<String, Any> {
             val map = HashMap<String, Any>()
             val hmsTrackSettings: HMSTrackSettings = hmssdk.hmsSettings
 
-            if (hmsTrackSettings.videoSettings != null) {
-                map["video_track_setting"] = HMSVideoTrackSettingsExtension.toDictionary(hmsTrackSettings.videoSettings)!!
+            hmsTrackSettings.audioSettings?.let { audioSettings ->
+                val settings = HMSAudioTrackSettingsExtension.toDictionary(audioSettings)
+                settings?.let {
+                    map["audio_track_setting"] = it
+                }
             }
 
-            if (hmsTrackSettings.audioSettings != null) {
-                map["audio_track_setting"] = HMSAudioTrackSettingsExtension.toDictionary(hmsTrackSettings.audioSettings)!!
+            hmsTrackSettings.videoSettings?.let {
+                val settings = HMSVideoTrackSettingsExtension.toDictionary(hmsTrackSettings.videoSettings)
+                settings?.let {
+                    map["video_track_setting"] = it
+                }
             }
 
             return map
         }
 
-        fun setTrackSettings(hmsAudioTrackHashMap: HashMap<String, Any?>?, hmsVideoTrackHashMap: HashMap<String, Any?>?): HMSTrackSettings {
-            var hmsAudioTrackSettings = HMSAudioTrackSettings.Builder()
+        fun setTrackSettings(audio: HashMap<String, Any?>?, video: HashMap<String, Any?>?): HMSTrackSettings {
+            val audioSettings = setAudioTrackSettings(audio)
 
-            hmsAudioTrackSettings.setUseHardwareAcousticEchoCanceler(false)
-            hmsAudioTrackSettings.setPhoneCallMuteState(PhoneCallState.DISABLE_MUTE_ON_VOIP_PHONE_CALL_RING)
+            val videoSettings = setVideoTrackSettings(video)
 
-            hmsAudioTrackHashMap?.let { audioHashMap ->
+            return HMSTrackSettings.Builder().audio(audioSettings.build()).video(videoSettings.build()).build()
+        }
+
+        private fun setAudioTrackSettings(audioMap: HashMap<String, Any?>?): HMSAudioTrackSettings.Builder {
+            var settingsBuilder = HMSAudioTrackSettings.Builder()
+
+            settingsBuilder.setUseHardwareAcousticEchoCanceler(false)
+            settingsBuilder.enableEchoCancellation(true)
+            settingsBuilder.enableNoiseSupression(true)
+            settingsBuilder.enableAutomaticGainControl(true)
+            settingsBuilder.setPhoneCallMuteState(PhoneCallState.DISABLE_MUTE_ON_VOIP_PHONE_CALL_RING)
+
+            audioMap?.let { audioHashMap ->
 
                 val initialState = HMSTrackInitStateExtension.getHMSTrackInitStatefromValue(audioHashMap["track_initial_state"] as String)
-                hmsAudioTrackSettings = hmsAudioTrackSettings.initialState(initialState)
+                settingsBuilder = settingsBuilder.initialState(initialState)
 
                 val useHardwareAcousticEchoCanceler =
                     audioHashMap["user_hardware_acoustic_echo_canceler"] as Boolean?
                 useHardwareAcousticEchoCanceler?.let { useHardware ->
-                    hmsAudioTrackSettings.setUseHardwareAcousticEchoCanceler(useHardware)
+                    settingsBuilder.setUseHardwareAcousticEchoCanceler(useHardware)
                 }
 
                 val phoneCallMuteState = audioHashMap["phone_call_mute_state"] as? String
                 phoneCallMuteState?.let { callMuteState ->
                     when (callMuteState) {
-                        "ENABLE_MUTE_ON_PHONE_CALL_RING" -> hmsAudioTrackSettings.setPhoneCallMuteState(PhoneCallState.ENABLE_MUTE_ON_PHONE_CALL_RING)
-                        else -> hmsAudioTrackSettings.setPhoneCallMuteState(PhoneCallState.DISABLE_MUTE_ON_VOIP_PHONE_CALL_RING)
+                        "ENABLE_MUTE_ON_PHONE_CALL_RING" -> settingsBuilder.setPhoneCallMuteState(PhoneCallState.ENABLE_MUTE_ON_PHONE_CALL_RING)
+                        else -> settingsBuilder.setPhoneCallMuteState(PhoneCallState.DISABLE_MUTE_ON_VOIP_PHONE_CALL_RING)
                     }
                 }
             }
 
-            var hmsVideoTrackSettings = HMSVideoTrackSettings.Builder()
-            if (hmsVideoTrackHashMap != null) {
-                val cameraFacing = getHMSCameraFacingFromValue(hmsVideoTrackHashMap["camera_facing"] as String?)
-                val disableAutoResize = hmsVideoTrackHashMap["disable_auto_resize"] as Boolean
-                val initialState = HMSTrackInitStateExtension.getHMSTrackInitStatefromValue(hmsVideoTrackHashMap["track_initial_state"] as String)
-                val forceSoftwareDecoder = hmsVideoTrackHashMap["force_software_decoder"] as Boolean
+            return settingsBuilder
+        }
 
-                if (cameraFacing != null) {
-                    hmsVideoTrackSettings = hmsVideoTrackSettings.cameraFacing(cameraFacing)
-                }
-                if (disableAutoResize != null) {
-                    hmsVideoTrackSettings = hmsVideoTrackSettings.disableAutoResize(disableAutoResize)
-                }
-                if (initialState != null) {
-                    hmsVideoTrackSettings = hmsVideoTrackSettings.initialState(initialState)
-                }
-                if (forceSoftwareDecoder != null) {
-                    hmsVideoTrackSettings = hmsVideoTrackSettings.forceSoftwareDecoder(forceSoftwareDecoder)
-                }
+        private fun setVideoTrackSettings(videoMap: HashMap<String, Any?>?): HMSVideoTrackSettings.Builder {
+            var settingsBuilder = HMSVideoTrackSettings.Builder()
+
+            videoMap?.let {
+                settingsBuilder = settingsBuilder.disableAutoResize(it["disable_auto_resize"] as Boolean)
+
+                settingsBuilder = settingsBuilder.forceSoftwareDecoder(it["force_software_decoder"] as Boolean)
+
+                settingsBuilder = settingsBuilder.cameraFacing(getHMSCameraFacingFromValue(it["camera_facing"] as String?))
+
+                settingsBuilder = settingsBuilder.initialState(HMSTrackInitStateExtension.getHMSTrackInitStatefromValue(it["track_initial_state"] as String))
             }
-
-            return HMSTrackSettings.Builder().audio(hmsAudioTrackSettings.build()).video(hmsVideoTrackSettings.build()).build()
+            return settingsBuilder
         }
 
         private fun getHMSCameraFacingFromValue(cameraFacing: String?): HMSVideoTrackSettings.CameraFacing {
             return when (cameraFacing) {
                 "back" -> HMSVideoTrackSettings.CameraFacing.BACK
-                "front" -> HMSVideoTrackSettings.CameraFacing.FRONT
                 else -> HMSVideoTrackSettings.CameraFacing.FRONT
             }
         }
