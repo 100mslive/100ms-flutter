a3b1fccf12170a463505c589ee6f3a894d081e46
Yogesh Singh <yogesh@100ms.live>
2023-08-24 19:54:11 +0530

    added default data type to result extension

--- packages/hmssdk_flutter/android/src/main/kotlin/live/hms/hmssdk_flutter/HMSResultExtension.kt
+++ packages/hmssdk_flutter/android/src/main/kotlin/live/hms/hmssdk_flutter/HMSResultExtension.kt
@@ -3,7 +3,7 @@ package live.hms.hmssdk_flutter
 class HMSResultExtension {
 
     companion object {
-        fun toDictionary(success: Boolean, data: Any?): HashMap<String, Any?> {
+        fun toDictionary(success: Boolean, data: Any? = null): HashMap<String, Any?> {
             val hashMap = HashMap<String, Any?>()
             hashMap["success"] = success
             hashMap["data"] = data
