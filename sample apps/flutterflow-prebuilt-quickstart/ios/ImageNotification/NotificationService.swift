b9da34348a76a010324fd04ceeae8b6396a2811b
Yogesh Singh <yogesh@100ms.live>
2023-12-04 19:23:56 +0530

    updated packages

--- sample apps/flutterflow-prebuilt-quickstart/ios/ImageNotification/NotificationService.swift
+++ sample apps/flutterflow-prebuilt-quickstart/ios/ImageNotification/NotificationService.swift
@@ -15,7 +15,7 @@ class NotificationService: UNNotificationServiceExtension {
           bestAttemptContent,
           withContentHandler: contentHandler)
     }
-    
+
     override func serviceExtensionTimeWillExpire() {
         // Called just before the extension will be terminated by the system.
         // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
--- sample apps/flutterflow-prebuilt-quickstart/lib/flutter_flow/flutter_flow_util.dart
+++ sample apps/flutterflow-prebuilt-quickstart/lib/flutter_flow/flutter_flow_util.dart
@@ -10,7 +10,6 @@ import 'package:url_launcher/url_launcher.dart';
 
 import '../main.dart';
 
-import 'lat_lng.dart';
 
 export 'lat_lng.dart';
 export 'place.dart';
