4e18524c19bd29936847eddc2a8cba482f51b504
Decoder07 <pushpam@100ms.live>
2024-04-24 13:51:36 +0530

    Fixed android bug

--- packages/hmssdk_flutter/lib/src/model/hls_stream_properties.dart
+++ packages/hmssdk_flutter/lib/src/model/hls_stream_properties.dart
@@ -10,7 +10,7 @@ class HLSStreamProperties {
 
   factory HLSStreamProperties.fromMap(Map<dynamic, dynamic> map) {
     return HLSStreamProperties(
-        rollingWindowTime: map['rolling_window_time'],
-        streamDuration: map['stream_duration']);
+        rollingWindowTime: map['rolling_window_time'] != null?map["rolling_window_time"].toDouble():null,
+        streamDuration: map['stream_duration']!= null?map["stream_duration"].toDouble():null);
   }
 }
