e6415123471f1de77e02d543021444c4ffae8ea0
Decoder07 <pushpam@100ms.live>
2024-05-27 15:30:03 +0530

    Added empty room screens

--- packages/hms_room_kit/lib/src/widgets/bottom_sheets/app_utilities_bottom_sheet.dart
+++ packages/hms_room_kit/lib/src/widgets/bottom_sheets/app_utilities_bottom_sheet.dart
@@ -379,7 +379,7 @@ class _AppUtilitiesBottomSheetState extends State<AppUtilitiesBottomSheet> {
                                   ? "Recording"
                                   : "Record",
                         ),
-                if (meetingStore.isNoiseCancellationAvailable &&
+                if (meetingStore.isNoiseCancellationAvailable && meetingStore.localPeer?.audioTrack != null &&
                     meetingStore.isMicOn)
                   MoreOptionItem(
                       onTap: () async {
