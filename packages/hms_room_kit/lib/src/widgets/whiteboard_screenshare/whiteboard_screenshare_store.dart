1b3c3ed3a023dd676cade777f79117cac7081dd0
Decoder07 <pushpam@100ms.live>
2024-06-06 17:47:04 +0530

    Removed pop up in full screen share

--- packages/hms_room_kit/lib/src/widgets/whiteboard_screenshare/whiteboard_screenshare_store.dart
+++ packages/hms_room_kit/lib/src/widgets/whiteboard_screenshare/whiteboard_screenshare_store.dart
@@ -2,13 +2,20 @@ library;
 
 ///Package imports
 import 'package:flutter/material.dart';
+import 'package:hms_room_kit/src/meeting/meeting_store.dart';
 
 ///[WhiteboardScreenshareStore] is a store that stores the state of the whiteboard screenshare
 class WhiteboardScreenshareStore extends ChangeNotifier {
+  MeetingStore meetingStore;
   bool isFullScreen = false;
+
+  WhiteboardScreenshareStore({required this.meetingStore}) {
+    isFullScreen = meetingStore.isScreenshareWhiteboardFullScreen;
+  }
 
   void toggleFullScreen() {
     isFullScreen = !isFullScreen;
+    meetingStore.isScreenshareWhiteboardFullScreen = isFullScreen;
     notifyListeners();
   }
 }
